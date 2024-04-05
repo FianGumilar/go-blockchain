89909578ebeeb15f9e1ef444eb34e1052b0ad9a6
FianGumilar <fiangumilar@gmail.com>
2024-04-05 22:06:48 +0700

    feat: genesis blockchain

--- domain/blockchain.go
+++ domain/blockchain.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -15,13 +16,14 @@ type Blockchain struct {
 func NewBlockchain() *Blockchain {
 	bc := LoadDatabase()
 	if len(bc.Chain) == 0 {
+		bc.CreateGenesis()
 		bc.CreateBlock(fmt.Sprintf("%x", [32]byte{}))
 	}
 
-	return bc
+	return &bc
 }
 
-func LoadDatabase() *Blockchain {
+func LoadDatabase() Blockchain {
 	f, err := os.OpenFile("database/blockchain.db", os.O_APPEND|os.O_RDWR, 0600)
 	if err != nil {
 		os.Exit(1)
@@ -41,10 +43,13 @@ func LoadDatabase() *Blockchain {
 			os.Exit(1)
 		}
 
+		if len(blockchain.Chain) > 0 && (blockchain.LatestBlock().Hash() != blockSerialized.Value.Header.PrevHash) {
+			log.Fatal("invalid block-chain database")
+		}
+
 		blockchain.Chain = append(blockchain.Chain, blockSerialized.Value)
 	}
-	return &blockchain
-
+	return blockchain
 }
 
 func (bc *Blockchain) CreateBlock(prevHash string) *Block {
@@ -55,11 +60,38 @@ func (bc *Blockchain) CreateBlock(prevHash string) *Block {
 	return b
 }
 
-func (bc *Blockchain) GiveMandate(from, to string, value int64) {
+func (bc *Blockchain) GiveMandate(from, to string, value int64) bool {
+	if bc.CalculateMandate(from) < value {
+		return false
+	}
+
 	m := NewMandate(from, to, value)
 	bc.Pool = append(bc.Pool, m)
+	return true
 }
 
 func (bc *Blockchain) LatestBlock() *Block {
 	return bc.Chain[len(bc.Chain)-1]
+}
+
+func (bc *Blockchain) CreateGenesis() {
+	m := NewMandate("Owner", "Investor", 10)
+	bc.Pool = append(bc.Pool, m)
+}
+
+func (bc *Blockchain) CalculateMandate(user string) int64 {
+	var total int64
+
+	for _, v := range bc.Chain {
+		for _, v2 := range v.Mandates {
+			if v2.To == user {
+				total += v2.Value
+			}
+
+			if v2.From == user {
+				total -= v2.Value
+			}
+		}
+	}
+	return total
 }
--- main.go
+++ main.go
@@ -9,8 +9,20 @@ import (
 func main() {
 	bc := domain.NewBlockchain()
 
-	bc.GiveMandate("BBCA", "Quuena", 1)
+	bc.GiveMandate("Owner", "Investor", 1)
+
+	if !bc.GiveMandate("Investor", "Quena", 1) {
+		log.Println("insufficient-mandate")
+	}
 	bc.CreateBlock(bc.LatestBlock().Hash())
+
+	if !bc.GiveMandate("Investor", "Ansuk", 1) {
+		log.Println("insufficient-mandate")
+	}
+	bc.CreateBlock(bc.LatestBlock().Hash())
+
+	log.Printf("Quena => :%v", bc.CalculateMandate("Quena"))
+	log.Printf("Ansuk => :%v", bc.CalculateMandate("Ansuk"))
 
 	data, _ := json.Marshal(bc)
 	log.Println(string(data))
